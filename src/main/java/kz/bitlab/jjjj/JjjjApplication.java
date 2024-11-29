f621dbfe0f9b3a8121e4e624acbac319ac7e7d2f
adilzhakypov <zakypovadil@gmail.com>
2024-11-29 19:53:49 +0500

    initial commit

---
+++ src/main/java/kz/bitlab/jjjj/JjjjApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.jjjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjjjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjjjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bitlab/jjjj/JjjjApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.jjjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjjjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
