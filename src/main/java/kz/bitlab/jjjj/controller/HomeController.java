677d93ecc3a0aa5bed728f5ba545a87420fc9aa3
adilzhakypov <zakypovadil@gmail.com>
2024-12-02 20:49:56 +0500

    UPD html

--- src/main/java/kz/bitlab/jjjj/controller/HomeController.java
+++ src/main/java/kz/bitlab/jjjj/controller/HomeController.java
@@ -2,9 +2,12 @@ package kz.bitlab.jjjj.controller;
 
 import kz.bitlab.jjjj.db.DBManager;
 import kz.bitlab.jjjj.entity.Student;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,4 +21,20 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/student/add")
+    public String studentAddPAge(){
+        return "studentadd";
+    }
+
+
+
+    @PostMapping("/student/add")
+    public String add(Student student) {
+        DBManager.addStudent(student);
+        return "redirect:/";
+    }
 }
+
+
+
+
--- src/main/java/kz/bitlab/jjjj/db/DBManager.java
+++ src/main/java/kz/bitlab/jjjj/db/DBManager.java
@@ -8,15 +8,38 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DBManager {
-    Scanner scanner = new Scanner(System.in);
+
 
     @Getter
     private static List<Student> students = new ArrayList<>();
+
 
     static {
         students.add(new Student(1L, "Adil", "A", 78, "A"));
         students.add(new Student(2L, "Sanzhar", "M", 34, "F"));
         students.add(new Student(3L, "Kuat", "Z", 65, "C"));
+    }
+
+    private static Long id = 5L;
+    public static void addStudent(Student student){
+    student.setId(id);
+    student.setMark(calculateMark(student.getExam()));
+    students.add(student);
+    id++;
+    }
+
+    public static String calculateMark(int exam){
+        if(exam >= 90){
+            return("A");
+        } else if (exam >= 75 && exam < 90) {
+            return("B");
+        } else if (exam >= 60 && exam < 75) {
+            return("C");
+        } else if (exam >= 50 && exam < 60) {
+            return("B");
+        } else {
+            return("F");
+        }
     }
 
 }
